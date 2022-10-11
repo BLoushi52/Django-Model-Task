4a065341b98020a64df352588ae73875e4807cb6
BLoushi52 <k.albloushii@hotmail.com>
2022-10-11 19:32:11 +0300

    fixed issues

--- restuarants/models.py
+++ restuarants/models.py
@@ -1,14 +1,13 @@
-from email.policy import default
-from unittest.util import _MAX_LENGTH
+
 from django.db import models
 
 # Create your models here.
 
 
-class Restuarants(models.Model):
+class Restuarant(models.Model):
 
-    name = models.CharField(max_length=30),
-    description = models.TextField(default=""),
-    opening_time = models.TimeField(),
-    closing_time = models.TimeField(),
-    created_at = models.DateTimeField(auto_now_add=True),
+    name = models.CharField(max_length=30)
+    description = models.TextField(default="")
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
