c054cc4c92ddeaf7f2e7a6908983f6dbf4edbf19
BLoushi52 <k.albloushii@hotmail.com>
2022-10-11 17:52:04 +0300

    created Restuarants model in restuarants app

--- restuarants/models.py
+++ restuarants/models.py
@@ -1,3 +1,13 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+
+class Restuarants(models.Model):
+
+    name = models.CharField(max_length=30),
+    description = models.CharField(max_length=""),
+    opening_time = models.TimeField(),
+    closing_time = models.TimeField(),
+    created_at = models.DateTimeField(),
